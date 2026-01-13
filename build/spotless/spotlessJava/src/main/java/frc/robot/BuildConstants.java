421e5dec4529aaa0e306e99fa5af090360200e2e
13600k <123127287+13600k@users.noreply.github.com>
2026-01-12 21:32:31 -0500

    Add team number and dev chassis tunerconstants

---
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Team-469-Robot-Code-2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "1c6341150980035478b0dd2dfab9362e1f9f94a9";
+  public static final String GIT_DATE = "2026-01-12 15:05:39 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-12 19:54:23 EST";
+  public static final long BUILD_UNIX_TIME = 1768265663870L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
