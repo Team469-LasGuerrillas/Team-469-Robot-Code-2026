3e0f44a3251bdf318e306eef1d0f1708b927f682
13600k <123127287+13600k@users.noreply.github.com>
2026-01-13 16:37:30 -0500

    Vision and motor AdvantageKit interface
    Created generic interfaces containing pertinent information for each generic hardware type.

---
+++ src/main/java/frc/lib/subsystems/interfaces/VisionIO.java
@@ -0,0 +1,71 @@
+package frc.lib.subsystems.interfaces;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Time;
+
+public interface VisionIO {
+
+    public record TrackedTarget(
+        Time timestamp,
+        Transform3d origin,
+        Angle tx,
+        Angle ty,
+        double area,
+        TargettingType targetType) {}
+
+    public record PoseObservation(
+        double timestamp,
+        double ambiguity,
+        double ta,
+        double tagCount,
+        Pose3d pose,
+        double[] stdDevs,
+        PoseObservationType type) {}
+
+    public enum TargettingType {
+        FIDUCIAL,
+        COLOR,
+        RETROREFLECTIVE,
+        OBJECT_DETECTION
+    }
+    public enum PoseObservationType {
+        MT1,
+        MT2
+    }
+    
+    @AutoLog
+    public class VisionInputs {
+        // General targetting
+        public int fiducialCount = 0;
+        public double ta = 0;
+        public TargettingType targettingType = TargettingType.RETROREFLECTIVE;
+        public boolean hasLatestFrame = false;
+
+        // Apriltag localization
+        public PoseObservation[] poseObservations;
+
+        // Object detection
+        public TrackedTarget[] trackedTargets;
+
+        // Performance
+        public Temperature cpuTemp = Temperature.ofBaseUnits(0, null);
+        public double totalLatencyMs = 0;
+        public double fps = 0;
+    }
+
+    default void readInputs(VisionInputsAutoLogged inputs) {}
+
+    default void setPoseRobotSpace(Pose3d cameraPose) {}
+
+    default void setRobotRotationUpdate(Rotation2d rotation, Rotation2d angularVelocity) {}
+
+    default void setPipelineIndex(int index) {}
+
+    default void setTagFiltersOverride(int[] tags) {}
+}
