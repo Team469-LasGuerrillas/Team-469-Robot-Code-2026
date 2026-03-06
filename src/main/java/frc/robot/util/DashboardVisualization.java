1e5eacb2faa4157014bb8d5f2b91932cfc02e263
RG2724 <rahulprahaladgururajan@gmail.com>
2026-03-05 20:24:47 -0500

    Created Dashboard visualization class
    Started working a little bit on it, specifically the part for putting robot pose on the field diagram.

---
+++ src/main/java/frc/robot/util/DashboardVisualization.java
@@ -0,0 +1,27 @@
+// package frc.robot.util;
+
+// import org.littletonrobotics.junction.Logger;
+
+// import edu.wpi.first.math.geometry.Pose2d;
+// import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+// import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
+// import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj2.command.Command;
+// import frc.lib.drivers.Elastic;
+
+// public class DashboardVisualization {
+//   private static String robotOnField = "Field";
+
+
+
+//   public static void setRobotPose(Pose2d robotPose2d) {
+//   Logger.recordOutput("Dashboard/Field", setRobotPose(robotPose2d));
+//   }
+
+//   public static void getRobotPose(Pose2d robotPose2d) {
+//   Logger.recordOutput("Dashboard/Field", getRobotPose(robotPose2d));
+//   }
+  
+//   }
+
