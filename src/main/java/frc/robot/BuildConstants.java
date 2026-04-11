1b9e50ec7c626988f806dead753b6b952bfa272b
Jimmy <jimmy.j.cao@gmail.com>
2026-04-11 14:46:50 -0400

    Change "spindexer" commands to run both motors

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-469-Robot-Code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 113;
-  public static final String GIT_SHA = "cb86427061f5cc056435afa47d366c7ff30f1824";
-  public static final String GIT_DATE = "2026-04-11 14:31:47 EDT";
+  public static final int GIT_REVISION = 114;
+  public static final String GIT_SHA = "8334f47d123bb9808e39e47eede22e3cf5586fb7";
+  public static final String GIT_DATE = "2026-04-11 14:41:56 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-11 14:41:32 EDT";
-  public static final long BUILD_UNIX_TIME = 1775932892103L;
+  public static final String BUILD_DATE = "2026-04-11 14:46:29 EDT";
+  public static final long BUILD_UNIX_TIME = 1775933189862L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/Spindexer.java
+++ src/main/java/frc/robot/subsystems/Spindexer.java
@@ -8,12 +8,13 @@ import frc.lib.subsystems.interfaces.MotorInputsAutoLogged;
 public class Spindexer extends SubsystemBase {
   private static Spindexer instance;
 
-  private final MotorIO lead;
-  private final MotorIO follower;
+  private final MotorIO floor;
+  private final MotorIO secondary;
   private final MotorInputsAutoLogged talonInputs = new MotorInputsAutoLogged();
   private ServoMotorSubsystemWithFollowersConfig.FollowerConfig followerConfig;
 
-  private double requestedDutycycle = 0;
+  private double requestedDutycycleFloor = 0;
+  private double requestedDutycycleSecondary = 0;
 
   public static Spindexer createinstance(MotorIO floorMotorIo, MotorIO secondaryMotorIo) {
     instance = new Spindexer(floorMotorIo, secondaryMotorIo);
@@ -25,18 +26,28 @@ public class Spindexer extends SubsystemBase {
   }
 
   private Spindexer(MotorIO floor, MotorIO secondary) {
-    this.lead = floor;
-    this.follower = secondary;
+    this.floor = floor;
+    this.secondary = secondary;
   }
 
-  public void setOpenLoopDutyCycle(double dutyCycle) {
-    requestedDutycycle = dutyCycle;
-    lead.setOpenLoopDutyCycle(requestedDutycycle);
+  public void setOpenLoopDutyCycleFloor(double dutyCycle) {
+    requestedDutycycleFloor = dutyCycle;
+    floor.setOpenLoopDutyCycle(requestedDutycycleFloor);
+  }
+
+  public void setOpenLoopDutyCycleSecondary(double dutyCycle) {
+    requestedDutycycleSecondary = dutyCycle;
+    secondary.setOpenLoopDutyCycle(requestedDutycycleSecondary);
+  }
+
+  public void setOpenLoopDutyCycleBoth(double dutyCycle) {
+    setOpenLoopDutyCycleFloor(dutyCycle);
+    setOpenLoopDutyCycleSecondary(dutyCycle);
   }
 
   @Override
   public void periodic() {
-    lead.readInputs(talonInputs);
+    floor.readInputs(talonInputs);
     // Logger.processInputs(getName() + "Motor", talonInputs);
   }
 }
