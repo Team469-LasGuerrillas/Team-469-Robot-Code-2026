6c5d955c38873b776b609ef5f956a9c74c07170d
Jimmy <jimmy.j.cao@gmail.com>
2026-04-30 11:01:59 -0500

    Slightly adjust ToF based on prac matches

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-469-Robot-Code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 143;
-  public static final String GIT_SHA = "ce519af1f405fef80d27b5086d905cee338ca444";
-  public static final String GIT_DATE = "2026-04-29 19:05:32 EDT";
+  public static final int GIT_REVISION = 144;
+  public static final String GIT_SHA = "f71cab6389c1caf8b8a387940faf8602a37224a5";
+  public static final String GIT_DATE = "2026-04-29 19:23:17 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-29 19:22:33 EDT";
-  public static final long BUILD_UNIX_TIME = 1777504953760L;
+  public static final String BUILD_DATE = "2026-04-30 12:01:13 EDT";
+  public static final long BUILD_UNIX_TIME = 1777564873812L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
